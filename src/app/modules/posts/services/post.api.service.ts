c59a7eee668074fe4fb5098968582d614ef6244f
Manuel Melgar <manuelmelgar0198@gmail.com>
2025-10-07 15:34:00 -0700

    feat(posts): implementar getAll() en PostApiService y prueba básica

--- src/app/modules/posts/services/post.api.service.ts
+++ src/app/modules/posts/services/post.api.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Post } from '../models/post.model';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostApiService {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiBaseUrl}/posts`;
 
-  constructor() { }
+  getAll(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.baseUrl);
+  }
+  
 }

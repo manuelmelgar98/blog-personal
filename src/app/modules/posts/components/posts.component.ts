6b0bdeeb7c50e76808214e15e93c06a7f447a743
Manuel Melgar <manuelmelgar0198@gmail.com>
2025-10-09 14:23:43 -0700

    feat(posts): integrar PostsComponent con Web Components via evento posts-changed

--- src/app/modules/posts/components/posts.component.ts
+++ src/app/modules/posts/components/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { PostApiService } from '../services/post.api.service';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Component({
   standalone: true,
@@ -12,38 +13,12 @@ export class PostsComponent implements OnInit {
   private api = inject(PostApiService);
 
   ngOnInit() {
-      const prueba = {
-        id: "4",
-        title: "Prueba 4",
-        content: "Esta es la cuarta prueba",
-        date: "07/10/2025",
-        categories: [1,2],
-        tags: [1,2]
-      }
-
-      // this.api.getAll().subscribe({
-      //   next: (posts) => console.log('API posts', posts),
-      //   error: (err) => console.error('Error API:', err)
-      // });
-
-      // this.api.getById("3").subscribe({
-      //   next: (post) => console.log('API post:', post),
-      //   error: (err) => console.error('Error API:', err)
-      // })
-    
-      // this.api.addPost(prueba).subscribe({
-      //   next: (post) => console.log(post),
-      //   error: (err) => console.error(err)
-      // })
-
-      // this.api.updatePost('4', prueba).subscribe({
-      //   next: (post) => console.log(post),
-      //   error: (err) => console.error(err)        
-      // })
-
-      // this.api.deletePost("4").subscribe({
-      //   next: (post) => console.log(post),
-      //   error: (err) => console.error(err)        
-      // })
+      this.api.getAllPosts().subscribe(posts => {
+        document.dispatchEvent(
+          new CustomEvent('posts-changed', {
+            detail: { action: 'load', posts }
+          })
+        );
+      });
   }
 }
